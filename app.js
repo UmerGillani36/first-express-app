6620b782f4b91238eeafd280cc8685df53011f8e
UmerGillani36 <syedomershahgillani@gmail.com>
2023-11-22 14:41:47 +0500

    404 page handled

--- app.js
+++ app.js
@@ -11,4 +11,8 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req,res,next)=>{
+    req.statusCode(404).send('<h4>404 Page not found</h4>')
+})
+
 app.listen(3000);
